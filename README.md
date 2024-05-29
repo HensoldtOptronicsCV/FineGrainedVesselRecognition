4dae556083f533f83278ee2624fbb65e7f987b3c
HeikoKarus <84770201+HeikoKarus@users.noreply.github.com>
2024-05-29 14:04:35 +0200

    Add download and sorting script

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # FineGrainedVesselRecognition
+
+This repository shows how to download the Military MARVEL Dataset and sorty it by class or superclass.
+
+Here are the direct links to the papers:\
+TODO
+
+The code can be excecuted using Python3.10 and the dependencies can be found in requirments.txt.
+
+Downloading Military MARVEL dataset:
+ In order to download the Military MARVEL dataset, download.py a Python script must be run. Set `SAVE_PHOTOGRAPHER = 1` if
+ you also want to save the photographer of each image.
+
+Sort images by class or superclass:
+ In order to sort the images of the Military MARVEL dataset, sort_by.py a Python script must be run. Set `SORT_BY = "class"` if you want to create the fine-grained dataset with 137 classes and `SORT_BY = "superclass"` to create the less fine-grained dataset with 11 classes.
+
+## Cite us
+
+If you use Military MARVEL in your research, please cite:
+
+  @PROCEEDINGS {Military_MARVEL,
+     author       = "XXX",
+     title        = "XXXX",
+     year         = "2024",
+     organization = "XXX"
+  }
---
+++ sort_by.py
@@ -0,0 +1,62 @@
+import os
+import sys
+from pathlib import Path
+import json
+import shutil
+
+
+SORT_BY = "class"  # or "superclass"
+IMAGE_DIR = os.path.join(os.getcwd(), "data", "images")
+FILE_TO_DOWNLOAD_FROM = "metadata.json"
+
+
+def create_sorted_dataset(
+    downloaded_ids: list[str],
+    annotation_data: list[dict[str, str]],
+    dataset_folder: str,
+):
+    """The create_sorted_dataset function loops through each image_id in downloaded_ids and searches for the
+    corresponding image_data in annotation_data. If the image_data is found, it creates a directory with the
+    name of the category in the dataset_folder if it doesn't already exist. It then copies the image file from
+    the IMAGE_DIR to the newly created category directory.
+
+    Args:
+        downloaded_ids (list[str]): A list of image IDs that have been downloaded.
+        annotation_data (list[dict[str, str]]): A list of dictionaries containing annotation data for each image.
+        dataset_folder (str): The path to the folder where the sorted dataset will be created.
+    """
+    for image_id in downloaded_ids:
+        image_data = next(
+            (item for item in annotation_data if item["image_id"] == image_id), None
+        )
+        if image_data is not None:
+            category = image_data[SORT_BY]
+            category_dir = os.path.join(dataset_folder, category)
+            if not os.path.exists(category_dir):
+                os.makedirs(category_dir)
+
+            shutil.copy(
+                os.path.join(IMAGE_DIR, image_id + ".jpg"),
+                os.path.join(category_dir, image_id + ".jpg"),
+            )
+
+
+def main():
+
+    downloaded_ids = [file.stem for file in Path(IMAGE_DIR).glob("*.jpg")]
+
+    with open(FILE_TO_DOWNLOAD_FROM, "r", encoding="utf-8") as annotation_file:
+        annotation_data = json.load(annotation_file)
+
+    dataset_folder = IMAGE_DIR.replace("images", SORT_BY + "_dataset")
+
+    if os.path.isdir(dataset_folder):
+        sys.exit("Dataset already exists, please delete it!")
+
+    os.makedirs(dataset_folder)
+
+    create_sorted_dataset(downloaded_ids, annotation_data, dataset_folder)
+
+
+if __name__ == "__main__":
+    main()
